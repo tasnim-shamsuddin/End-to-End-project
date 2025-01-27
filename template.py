1fc6bf497ed0a4900cb84d253d967b2ecc93d2a5
Tasnim Shamsuddin <tasnim.india@gmail.com>
2025-01-27 12:13:46 +0530

    implemented pipline

---
+++ template.py
@@ -0,0 +1,9 @@
+import os 
+from pathlib import Path 
+
+package_name="Diamond_Price_Prediction"
+
+list_of_files= [
+    "github/workflows/.gitkeep",
+    f"src/{package_name}/__init__.py", 
+    f"src"]
