98adc37ee381eb908bfd0a12160fb4f64462f7ff
Tasnim Shamsuddin <tasnim.india@gmail.com>
2025-02-02 11:08:18 +0530

    completed the model training

---
+++ src_DiamondPrediction/components/data_ingestion.py
@@ -0,0 +1,8 @@
+class DataTransformation:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+        
+    
+    def get_data_transformation(self):
+        logging.info("data ingestion started")
+        
